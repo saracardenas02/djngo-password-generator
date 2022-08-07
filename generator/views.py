ca2f6da61efdf2dbf50d422fc1a85e586e6360b7
Sara Cárdenas Meneses <smcm_02@hotmail.com>
2022-08-06 19:55:50 -0500

    first commit

---
+++ generator/views.py
@@ -0,0 +1,30 @@
+import random
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def home(request):
+    return render(request, 'generator/home.html')
+
+def about(request):
+    return render(request, 'generator/about.html')
+
+def password(request):
+
+    characters = list('abcdefghijklmnoprstuvwxyz')
+    generated_password = ''
+    length = int(request.GET.get('length'))
+
+    if request.GET.get('uppercase'):
+        characters.extend(list('ABCDEFGHIJKLMNOPQRSTUVWXYZ'))
+
+    if request.GET.get('special'):
+        characters.extend(list('!#$%&/.¡*_+-,@'))
+
+    if request.GET.get('number'):
+        characters.extend(list('0123456789'))
+
+    for x in range(length):
+        generated_password += random.choice(characters)
+
+    return render(request, 'generator/password.html', {'password': generated_password})
